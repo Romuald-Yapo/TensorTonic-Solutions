c19e07ea2bb0c1ba61a303f96fb1083df1a31979
Jean YAPO <114715760+Romuald-Yapo@users.noreply.github.com>
2026-03-16 12:19:15 +0100

    Update solution for entropy-node

--- entropy-node/entropy-node.py
+++ entropy-node/entropy-node.py
@@ -5,7 +5,7 @@ def entropy_node(y):
     Compute entropy for a single node using stable logarithms.
     """
     # Write code here
-    values, counts  = np.unique(y, return_counts= True)
+    _, counts  = np.unique(y, return_counts= True)
     
     probs = counts / len(y)
     #useless conditions but might be useful if you took as input directly the probabilities (e.g [0.1,0.2,0.7,0.0])
